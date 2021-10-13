cdef24efb62afe68cb7f600bb1aa2a88a79ba1d8
silentknight <chitnisapoorv@gmail.com>
2021-10-13 12:42:26 +0530

    deque empty commit

---
+++ deque.c
@@ -0,0 +1,7 @@
+// A dequeue is an ordered set of elements in which elements
+// may be inserted or retrieved from either end. Using an array
+// simulate a dequeue of characters and the operations retrieve
+// left, retrieve right, insert left, insert right. Exceptional
+// conditions such as dequeue full or empty should be indicated.
+// Two pointers (namely, left and right) are needed in this
+// simulation.
