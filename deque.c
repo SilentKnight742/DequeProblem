de4a957f6e77b997aaeb8fd4a701bf8ea37b7cc9
silentknight <chitnisapoorv@gmail.com>
2021-10-13 17:35:03 +0530

    basic structure

--- deque.c
+++ deque.c
@@ -5,3 +5,24 @@
 // conditions such as dequeue full or empty should be indicated.
 // Two pointers (namely, left and right) are needed in this
 // simulation.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 500
+
+void retrieveLeft()
+{
+}
+void retrieveRight()
+{
+}
+void insertLeft()
+{
+}
+void insertRight()
+{
+}
+int main()
+{
+    char dequeue[MAX];
+}
